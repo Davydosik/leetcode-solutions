319abec39dbdcac7db3587fd16e99ab7bfc5ff9c
Davydosik <81441980+Davydosik@users.noreply.github.com>
2026-08-02 18:26:49 +0300

    Time: 7 ms (43.83%), Space: 15.3 MB (43.28%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.py
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.py
@@ -12,15 +12,11 @@ class Solution(object):
         def helper(left, right):
             if left > right:
                 return None
-            
+                
             mid = (right + left) // 2
-            
             root = TreeNode(nums[mid])
-            
             root.left = helper(left, mid - 1) 
-            
             root.right = helper(mid + 1, right) 
-            
             return root
 
         return helper(0, len(nums) - 1)
