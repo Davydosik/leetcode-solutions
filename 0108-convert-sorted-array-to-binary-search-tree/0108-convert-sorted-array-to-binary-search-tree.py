176483f2ca46fbb9d81c771979b0c1e1f2251b02
Davydosik <81441980+Davydosik@users.noreply.github.com>
2026-08-02 18:27:15 +0300

    Time: 4 ms (78.47%), Space: 15.3 MB (43.28%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.py
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.py
@@ -6,17 +6,23 @@
 #         self.right = right
 class Solution(object):
     def sortedArrayToBST(self, nums):
+
         left = 0
+        
         right = len(nums) - 1
         
         def helper(left, right):
             if left > right:
                 return None
-                
+
             mid = (right + left) // 2
+
             root = TreeNode(nums[mid])
+
             root.left = helper(left, mid - 1) 
+
             root.right = helper(mid + 1, right) 
+
             return root
 
         return helper(0, len(nums) - 1)
